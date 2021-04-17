e9c1400324c12c45a9708bb952e727e550b9c887
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-04-17 19:53:24 -0300

    Criação de módulo para distribuição

---
+++ 2_compartilhando_seu_codigo/.ipynb_checkpoints/iteratelist-checkpoint.py
@@ -0,0 +1,19 @@
+"""
+Este é o módulo "iteratelist.py", e fornece uma função chamada
+print_lol() que imprime listas que podem ou não
+incluir listas aninhadas
+"""
+def print_lol(the_list):
+    """
+    Esta função requer um argumento posicional
+    chamdo "the_list", que é qualquer lista
+    Python(de possíveis listas aninhadas).
+    Cada item de dados na lista fornecida
+    é (recursivamente) impresso na tela em
+    sua própria linha
+    """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
---
+++ 2_compartilhando_seu_codigo/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'iteratelist',
+    version = '1.0.0',
+    py_modules = ['iteratelist'],
+    author = 'wmfinamore',
+    author_email = 'finamore.wm@gmail.com',
+    description = 'A simple printer of nested lists',
+)
+
+# No prompt de comando usar > setup.py sdist
+
+# Em seguida, para instalar use > setup.py install
