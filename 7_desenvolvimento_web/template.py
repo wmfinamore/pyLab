b2dce530abc068d081c103442fa2c4aa2de21843
Willian Moreira Finamore <wmfinamore@hotmail.com>
2021-06-01 22:27:06 -0300

    Aberto o projeto no pyCharm. Realizados testes com o modulo template

--- 7_desenvolvimento_web/template.py
+++ 7_desenvolvimento_web/template.py
@@ -6,18 +6,18 @@ def start_response(resp="text/html"):
 
 
 def include_header(the_title):
-    with open('template/header.html') as headf:
+    with open('template\header.html') as headf:
         head_text = headf.read()
     header = Template(head_text)
     return(header.substitute(title=the_title))
 
 
 def include_footer(the_links):
-    with open('templates/footer.html') as footf:
+    with open('template/footer.html') as footf:
         foot_text = footf.read()
     link_string = ''
     for key in the_links:
-        link_string += '<a href"' + the_links[key] + '">"' + key + '</a>$nbsp;$nbp,$nbsp,$nbsp;
+        link_string += '<a href"' + the_links[key] + '">"' + key + '</a>$nbsp;$nbp,$nbsp,$nbsp;'
     footer = Template(foot_text)
     return(footer.substitute(links=link_string))
 
